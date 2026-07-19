baa50d4705b54fab21a4bc1db14166f78cf05364
GSvegetable <3301296046@qq.com>
2026-07-19 18:43:04 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# 你的麦克机器人 Token
+BOT_TOKEN = "8952353448:AAHWPN_ViQI4xqtQUUyvFt-kjPC4uDDJNV8"
+
+# 你的数字 ID（必须是这两个其中之一，才能上传视频）
+ADMIN_IDS = [7857605443, 7867520461]
+
+# 数据库文件路径（PythonAnywhere 上固定的）
+DB_PATH = '/home/gsyxyc/mysite/videos.db'
